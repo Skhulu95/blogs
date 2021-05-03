ce311b6043a2b1a07855ca5f4b2d518e360936a6
zoeengel <zengel1706@gmail.com>
2021-05-03 14:27:47 +0200

    blog-cards

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,5 +1,142 @@
 <template>
-  <div class="blog">
-    <h1>This is the blog page</h1>
+
+<div class="blog-container">
+   <div class="q-pa-md row items-start q-gutter-md">
+    <q-card class="my-card">
+      <img src="https://cdn.quasar.dev/img/mountains.jpg">
+
+      <q-card-section>
+        <div class="text-h6">Our Changing Planet</div>
+        <div class="text-subtitle2">by John Doe</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        hello there
+        {{ lorem }}
+      </q-card-section>
+      <div class="readmorebtn">
+      <q-btn color="secondary" label="Read" />
+      </div>
+    </q-card>
   </div>
+
+  <div class="q-pa-md row items-start q-gutter-md">
+    <q-card class="my-card">
+      <img src="https://cdn.quasar.dev/img/mountains.jpg">
+
+      <q-card-section>
+        <div class="text-h6">Our Changing Planet</div>
+        <div class="text-subtitle2">by John Doe</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        hello there
+        {{ lorem }}
+      </q-card-section>
+      <div class="readmorebtn">
+      <q-btn color="secondary" label="Read" />
+      </div>
+    </q-card>
+  </div>
+
+  <div class="q-pa-md row items-start q-gutter-md">
+    <q-card class="my-card">
+      <img src="https://cdn.quasar.dev/img/mountains.jpg">
+
+      <q-card-section>
+        <div class="text-h6">Our Changing Planet</div>
+        <div class="text-subtitle2">by John Doe</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        hello there
+        {{ lorem }}
+      </q-card-section>
+      <div class="readmorebtn">
+      <q-btn color="secondary" label="Read" />
+      </div>
+    </q-card>
+  </div>
+</div>
+
+<div class="blog-container">
+   <div class="q-pa-md row items-start q-gutter-md">
+    <q-card class="my-card">
+      <img src="https://cdn.quasar.dev/img/mountains.jpg">
+
+      <q-card-section>
+        <div class="text-h6">Our Changing Planet</div>
+        <div class="text-subtitle2">by John Doe</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        hello there
+        {{ lorem }}
+      </q-card-section>
+      <div class="readmorebtn">
+      <q-btn color="secondary" label="Read" />
+      </div>
+    </q-card>
+  </div>
+
+  <div class="q-pa-md row items-start q-gutter-md">
+    <q-card class="my-card">
+      <img src="https://cdn.quasar.dev/img/mountains.jpg">
+
+      <q-card-section>
+        <div class="text-h6">Our Changing Planet</div>
+        <div class="text-subtitle2">by John Doe</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        hello there
+        {{ lorem }}
+      </q-card-section>
+      <div class="readmorebtn">
+      <q-btn color="secondary" label="Read" />
+      </div>
+    </q-card>
+  </div>
+
+  <div class="q-pa-md row items-start q-gutter-md">
+    <q-card class="my-card">
+      <img src="https://cdn.quasar.dev/img/mountains.jpg">
+
+      <q-card-section>
+        <div class="text-h6">Our Changing Planet</div>
+        <div class="text-subtitle2">by John Doe</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        hello there
+        {{ lorem }}
+      </q-card-section>
+      <div class="readmorebtn">
+      <q-btn color="secondary" label="Read" />
+      </div>
+    </q-card>
+  </div>
+</div>
 </template>
+
+<style>
+.category{
+  display: flex;
+  flex-flow: column;
+}
+.blog-container{
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  padding: 5em;
+  background: #69696917 ;
+}
+.readmorebtn{
+  padding-bottom: 8px;
+  text-align: end;
+}
+.my-card{
+  width:330px;
+  height: auto;
+}
+</style>
